b1aff97da91f2aa627acd6c4de5f0a0d4076ad18
Bryan Tai <redhatpieman@gmail.com>
2018-03-12 14:31:21 -0700

    Added sound when ball hits player. Fixed collision checking

--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 public class PlayerView : PongeElement
 {
     public PlayerModel model;
+    private AudioSource audioSource;
 
-   
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    //Should only ever collide with Ball
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSource.Play();   
+    }
 }
